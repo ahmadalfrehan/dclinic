0c547f15efc8513f5723da2f24b7ce4735857dd9
ahmad-frehan <ahmad-frehan@futurecode-co.com>
2023-12-23 11:38:55 +0300

    init

--- lib/domain/entites/patient.dart
+++ lib/domain/entites/patient.dart
@@ -1,13 +1,17 @@
+import 'package:dclinic/domain/entites/treatment.dart';
+
 abstract class Patient {
   String? name;
   String? phone;
   String? address;
   int? patientId;
+  List<Treatment> treatment = [];
 
   Patient({
-    required  this.name,
-    required  this.phone,
-    required  this.address,
-    required  this.patientId,
+    required this.name,
+    required this.phone,
+    required this.address,
+    required this.patientId,
+    required this.treatment,
   });
 }
