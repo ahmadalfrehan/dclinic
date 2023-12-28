ddf115315fbc8a4980040612a57e8aabd84c871b
ahmad-frehan <ahmad-frehan@futurecode-co.com>
2023-12-28 16:14:20 +0300

    init

--- lib/Presentation/Widgets/addPatient.dart
+++ lib/Presentation/Widgets/addPatient.dart
@@ -3,18 +3,24 @@ import 'package:dclinic/conf/theme/app-themes.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
+import '../../conf/style/app-text-style.dart';
+
 class AddPatient extends GetView<HomeController> {
   const AddPatient({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container
-      (
-      height: Get.height/2,
-      width: Get.width/2,
+    return Container(
+      height: Get.height / 2,
+      width: Get.width / 2,
       decoration: BoxDecoration(
         color: AppTheme.white242,
-
+      ),
+      child: Column(
+        children: [
+          Text('Add Patient',
+              style: AppTextStyle.textStyleWith500Weight15SizeDarkBlueColor),
+        ],
       ),
     );
   }
